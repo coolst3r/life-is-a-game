395ea01e62d78c5ed89168bf128aec84bcc0fadc
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-01-07 19:45:35 +0000

    Add files via upload

---
+++ assets/SPAWNRANDOM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPAWNRANDOM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(
+           Random.Range(0F, 1F),
+           Random.Range(0F, 1F),
+          0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
